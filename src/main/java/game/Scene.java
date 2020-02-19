bb4e8c773725382fca53b0b49ae2fbe6cf98276b
Sophia Konovalova <sophia.konovalova2@gmail.com>
2020-02-19 15:41:35 +0100

    added some comments

--- src/main/java/game/Scene.java
+++ src/main/java/game/Scene.java
@@ -1,5 +1,6 @@
 package game;
 
+//you have to import the external library into your project structure YOURSELF, and then add it to your class path
 import com.google.gson.Gson;
 
 public class Scene {
