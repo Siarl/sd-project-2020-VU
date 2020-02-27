95c3550737ea4781b4a2fa5fc4c7deb3fe3da3b9
Sophia Konovalova <sophia.konovalova2@gmail.com>
2020-02-27 10:12:55 +0100

    added a gson tutorial thing for myself

--- src/main/java/game/Scene.java
+++
@@ -1,8 +0,0 @@
-package game;
-
-//you have to import the external library into your project structure YOURSELF, and then add it to your class path
-import com.google.gson.Gson;
-
-public class Scene {
-
-}
