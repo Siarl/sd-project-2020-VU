5abd0c1c2766ee4393c4bb57a5e130beab9363ba
Sophia Konovalova <sophia.konovalova2@gmail.com>
2020-02-27 10:30:45 +0100

    added some classes

--- src/main/java/game/Main.java
+++ src/main/java/game/Main.java
@@ -1,5 +1,7 @@
 package game;
 
+import game.scene.*;
+
 public class Main {
 	public static void main(String[] args){
 		System.out.println("Henlo");
--- src/main/java/game/scene/Scene.java
+++ src/main/java/game/scene/Scene.java
@@ -1,5 +1,8 @@
-package game;
+package game.scene;
 
 public class Scene {
-	
+	private Items[] items;
+	private Characters[] characters;
+	private String nameScene;
+	private int sceneID;
 }
